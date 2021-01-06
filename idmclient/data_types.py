89940f8b4b393a042f092d87e78903c40199fd1b
claashk <claashk@gmx.de>
2021-01-06 01:42:48 +0100

    Added signed integer data type

--- idmclient/data_types.py
+++ idmclient/data_types.py
@@ -3,6 +3,7 @@ from modbusclient import AtomicType
 FLOAT = AtomicType("f", nan=-1., swap_words=True)
 UCHAR = AtomicType("B", nan=0xFF)
 U16 = AtomicType("H", nan=0xFFFF)
+I16 = AtomicType("h")
 WORD = U16
 U32 = AtomicType("I", nan=0xFFFFFFFF)
 
